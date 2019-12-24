6e855718a69f1186fda7e718d870099c6f582620
Daniel Jackson <d95jackson@gmail.com>
2019-12-24 00:57:09 +0000

    add day01 solutions

---
+++ solutions/day01/main.js
@@ -0,0 +1,41 @@
+const fs = require('fs-extra')
+const path = require('path');
+
+
+const calculateFuel = n => {
+  const fuel = Math.floor(n / 3) - 2;
+
+  return fuel > 0 ? fuel : 0
+}
+
+const calculateTotalFuel = n => {
+  const fuel = calculateFuel(n);
+
+  if(fuel > 0) {
+    return fuel + calculateTotalFuel(fuel);
+  }
+  return 0;
+}
+
+const a = () => {
+  const inputs = fs.readFileSync(path.resolve(__dirname, 'input.txt'), 'utf-8').split('\n')
+
+  const answer = inputs.reduce((acc, n) => {
+    return acc + Math.floor(n/3) - 2
+  }, 0)
+
+  console.log(`a = ${answer}`)
+}
+
+const b = () => {
+  const inputs = fs.readFileSync(path.resolve(__dirname, 'input.txt'), 'utf-8').split('\n')
+
+  const answer = inputs.reduce((acc, n) => {
+    return acc + calculateTotalFuel(n);
+  }, 0)
+
+  console.log(`b = ${answer}`)
+}
+
+a();
+b();
