092ef3d2dcb0a33e213819c5a5880624b420ce6d
Daniel Jackson <d95jackson@gmail.com>
2019-12-26 16:34:11 +0000

    apparently it works, although test case for part b doesn't exactly work...

--- solutions/day05/main.js
+++ solutions/day05/main.js
@@ -2,10 +2,26 @@ const { getInputs } = require('../../utils/files')
 
 const inputs =  getInputs(5)[0]
 
-const runOpcodeInstructions = (instructions, defaultInput) => {
+// ==== Opcode computer V2 ====
+const POSITION_MODE = 0
+const IMMEDIATE_MODE = 1
+
+let opcodeOutputs = []
+const getOpcodeOutputs = () => opcodeOutputs
+
+const runOpcodeInstructions = (instructions, defaultInput, isSilentOutput = false) => {
+  if(typeof instructions === 'string') instructions = instructions.split(',').map(Number)
+
+  opcodeOutputs = []
+
   for(var head = 0; head < instructions.length; ) {
     const instruction = instructions[head];
     const opcode = Number(String(instruction).slice(-2))
+
+    const paramModes = String(instruction).slice(0, String(instruction).length - 2).split('').map(Number).reverse()
+    const c = paramModes[0] || POSITION_MODE
+    const b = paramModes[1] || POSITION_MODE
+    const a = paramModes[2] || POSITION_MODE
 
     // input
     if(opcode === 3) {
@@ -13,28 +29,42 @@ const runOpcodeInstructions = (instructions, defaultInput) => {
       head += 2;
     // output
     } else if (opcode === 4) { 
-      console.log(`diagnostic check: ${instructions[instructions[head + 1]]}`)
+      const output = instructions[instructions[head + 1]]
+      if(!isSilentOutput) {
+        console.log(`diagnostic check: ${output}`)
+      }
+      opcodeOutputs.push(output)
       head += 2;
     // add or multiply
-    }  else if (opcode === 1 || opcode === 2) {
-      const paramModes = String(instruction).slice(0, String(instruction).length - 2).split('').map(Number).reverse()
-      const c = paramModes[0] || 0
-      const b = paramModes[1] || 0
-      const a = paramModes[2] || 0
-
+    } else if (opcode === 1 || opcode === 2 || opcode === 7 || opcode === 8) {
       // 0 = position; 1 = immediate
-      const v1 = c === 0 ? instructions[instructions[head + 1]] : instructions[head + 1]
-      const v2 = b === 0 ? instructions[instructions[head + 2]] : instructions[head + 2]
-      const result = opcode === 1 ? v1 + v2 : v1 * v2
+      const v1 = c === POSITION_MODE ? instructions[instructions[head + 1]] : instructions[head + 1]
+      const v2 = b === POSITION_MODE ? instructions[instructions[head + 2]] : instructions[head + 2]
 
-      if(a === 0) {
-        instructions[Number(instructions[head + 3])] = result
-      } else {
-        instructions[head + 3] = result
+      let output
+      switch (opcode) {
+        case 1: output = v1 + v2; break;
+        case 2: output = v1 * v2; break;
+        case 7: output = v1 < v2 ? 1 : 0; break;
+        case 8: output = v1 === v2 ? 1 : 0; break;
       }
 
+      const outputPosition = a === POSITION_MODE ? instructions[head + 3] : head + 3
+      instructions[outputPosition] = output
+
       head += 4;
-      // return
+    // jumps
+    } else if (opcode === 5 || opcode === 6) {
+      const v1 = c === POSITION_MODE ? instructions[instructions[head + 1]] : instructions[head + 1]
+      const v2 = b === POSITION_MODE ? instructions[instructions[head + 2]] : instructions[head + 2]
+
+      if((opcode === 5 && v1 !== 0) || (opcode === 6 && v1 === 0)) {
+        head = v2
+      } else {
+        head += 3;
+      }
+
+    // return
     } else if (opcode === 99) {
       break;
     } else {
@@ -43,6 +73,7 @@ const runOpcodeInstructions = (instructions, defaultInput) => {
     }
   }
 }
+// ==== Opcode computer ====
 
 const a = () => {
   const instructions = inputs.split(',').map(Number)
@@ -63,4 +94,9 @@ var runningAsScript = !module.parent;
 if(runningAsScript) {
   a();
   b();
+}
+
+module.exports = {
+  getOpcodeOutputs,
+  runOpcodeInstructions
 }
--- solutions/day05/main.spec.js
+++ solutions/day05/main.spec.js
@@ -1,5 +1,6 @@
 const chai = require('chai')
 const expect = chai.expect
+const { getOpcodeOutputs, runOpcodeInstructions } = require('./main')
 
 describe('day 05', () => {
   describe('part a examples', () => {
@@ -7,6 +8,102 @@ describe('day 05', () => {
   })
 
   describe('part b examples', () => {
-    // tests
+    // opcode instruction 7 and 8 test cases
+    const equalityCases = [
+      { 
+        instructions: "3,9,8,9,10,9,4,9,99,-1,8",
+        expected: [
+          { input: 0, output: 0 },
+          { input: 7, output: 0 },
+          { input: 8, output: 1 },
+        ]
+      },{ 
+        instructions: "3,9,7,9,10,9,4,9,99,-1,8",
+        expected: [
+          { input: 4, output: 1 },
+          { input: 6, output: 1 },
+          { input: 8, output: 0 },
+          { input: 12, output: 0 },
+        ]
+      },{ 
+        instructions: "3,3,1108,-1,8,3,4,3,99",
+        expected: [
+          { input: 3, output: 0 },
+          { input: 7, output: 0 },
+          { input: 8, output: 1 },
+          { input: 11, output: 0 },
+        ]
+      },{ 
+        instructions: "3,3,1107,-1,8,3,4,3,99",
+        expected: [
+          { input: 2, output: 1 },
+          { input: 6, output: 1 },
+          { input: 8, output: 0 },
+          { input: 9, output: 0 },
+        ]
+      }
+    ]
+
+    equalityCases.forEach(({ instructions, expected }) => {
+      describe(`instructions "${instructions}"`, () => {
+        expected.forEach(({ input, output }) => {
+          it(`with input "${input}" should output "${output}"`, () => {
+            runOpcodeInstructions(instructions, input, true)
+            expect(getOpcodeOutputs()[0]).to.equal(output)
+          })
+        })
+      })
+    })
+
+    // jump test cases 
+    const jumpCases = [
+      { 
+        /*
+          3,12 // 0
+          6,12,15 // 2
+          1,13,14,13 // 5
+          4,13 // 9
+          99 // 11
+          -1 // 12
+          0 // 13
+          1
+          9 // 15
+        */
+        instructions: "3,12,6,12,15,1,13,14,13,4,13,99,-1,0,1,9",
+        expected: [
+          { input: -4, output: 1 },
+          { input: 0, output: 0 },
+          { input: 3, output: 1 },
+          { input: 8, output: 1 },
+        ]
+      },{ 
+        instructions: "3,3,1105,-1,9,1101,0,0,12,4,12,99,1",
+        expected: [
+          { input: -3, output: 1 },
+          { input: 0, output: 0 },
+          { input: 1, output: 1 },
+          { input: 9, output: 1 },
+        ]
+      }, { 
+        instructions: "3,21,1008,21,8,20,1005,20,22,107,8,21,20,1006,20,31,1106,0,36,98,0,0,1002,21,125,20,4,20,1105,1,46,104,999,1105,1,46,1101,1000,1,20,4,20,1105,1,46,98,99",
+        expected: [
+          { input: 7, output: 999 },
+          { input: 8, output: 1000 },
+          { input: 9, output: 1001 },
+          { input: 1001222, output: 1001 },
+        ]
+      }
+    ]
+
+    jumpCases.forEach(({ instructions, expected }) => {
+      describe(`instructions "${instructions}"`, () => {
+        expected.forEach(({ input, output }) => {
+          it(`with input "${input}" should output "${output}"`, () => {
+            runOpcodeInstructions(instructions, input, true)
+            expect(getOpcodeOutputs()[0]).to.equal(output)
+          })
+        })
+      })
+    })
   })
 })
