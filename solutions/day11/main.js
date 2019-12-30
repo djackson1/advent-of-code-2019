220b1e64fca7e9ffdf61caa647414b8d618e9b70
Daniel Jackson <d95jackson@gmail.com>
2019-12-30 17:21:43 +0000

    day 11 part b

--- solutions/day11/main.js
+++ solutions/day11/main.js
@@ -9,11 +9,6 @@ const FACING = {
   3: { x: -1, y: 0 },
 }
 
-//   UP: 0,
-//   RIGHT: 1,
-//   DOWN: 2,
-//   LEFT: 3
-// }
 const DIR = {
   LEFT: 0,
   RIGHT: 1,
@@ -24,47 +19,94 @@ const COLOR = {
   WHITE: 1,
 }
 
-const a = () => {
-  const computer = new Opcode(input, { isSilent: true })
-  computer.addInput(0)
+class Robot {
+  constructor (instructions, initialInput) {
+    this.__grid = {}
+    this.__position = { x: 0, y: 0 }
+    this.__facing = 0
 
-  const grid = {}
-  const robot = { x: 0, y: 0, facing: 0 }
-
-  while (true) {
-    const color = computer.run()
-    if (computer.isFinished) break
-
-    const dir = computer.run().value
-
-    // paint current
-    grid[`${robot.x}-${robot.y}`] = color.value
-
-    // turn
-    if (dir === DIR.LEFT) {
-      robot.facing -= 1
-      if (robot.facing < 0) robot.facing += 4
-    } else {
-      robot.facing += 1
-      if (robot.facing > 3) robot.facing -= 4
-    }
-
-    // move
-    const movement = FACING[robot.facing]
-    robot.x += movement.x
-    robot.y += movement.y
-
-    // get new color underneath for input
-    const colorUnder = grid[`${robot.x}-${robot.y}`]
-
-    computer.addInput(colorUnder || COLOR.BLACK)
+    this.__computer = new Opcode(instructions, { isSilent: true })
+    this.__computer.addInput(initialInput)
   }
-  // computer.runUntilFinished()
 
-  console.log('a', Object.keys(grid).length)
+  paint (color) {
+    this.__grid[`${this.__position.x}~${this.__position.y}`] = color
+  }
+
+  turn (dir) {
+    this.__facing += (dir === DIR.LEFT ? -1 : 1)
+    this.__facing = (this.__facing + 4) % 4
+  }
+
+  moveForward () {
+    const movement = FACING[this.__facing]
+    this.__position.x += movement.x
+    this.__position.y += movement.y
+  }
+
+  getCurrentColor () {
+    return this.__grid[`${this.__position.x}~${this.__position.y}`] || COLOR.BLACK
+  }
+
+  get grid () {
+    return this.__grid
+  }
+
+  drawGrid () {
+    const { minX, maxX, minY, maxY } = Object.keys(this.__grid).reduce(({ minX, maxX, minY, maxY }, xy) => {
+      const [x, y] = xy.split('~').map(Number)
+
+      return {
+        minX: x < minX ? x : minX,
+        maxX: x > maxX ? x : maxX,
+        minY: y < minY ? y : minY,
+        maxY: y > maxY ? y : maxY,
+      }
+    }, { minX: 0, maxX: 0, minY: 0, maxY: 0 })
+
+    for (let y = minY; y <= maxY; y++) {
+      const strTokens = []
+      for (let x = minX; x <= maxX; x++) {
+        const token = this.__grid[`${x}~${y}`]
+
+        strTokens.push(token ? '█' : ' ')
+      }
+      console.log(strTokens.join(''))
+    }
+  }
+
+  runUntilFinished () {
+    while (true) {
+      const color = this.__computer.run()
+      if (this.__computer.isFinished) break
+
+      const dir = this.__computer.run()
+
+      this.paint(color.value)
+      this.turn(dir.value)
+      this.moveForward()
+
+      const colorUnder = this.getCurrentColor()
+
+      this.__computer.addInput(colorUnder)
+    }
+  }
+}
+
+const a = () => {
+  const robot = new Robot(input, 0)
+
+  robot.runUntilFinished()
+
+  console.log('a', Object.keys(robot.grid).length)
 }
 const b = () => {
+  const robot = new Robot(input, 1)
+
+  robot.runUntilFinished()
+
   console.log('b = ?')
+  robot.drawGrid()
 }
 
 var runningAsScript = !module.parent
