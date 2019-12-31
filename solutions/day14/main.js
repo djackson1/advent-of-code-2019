9858317e7614bdc74ad2fc10f1f432ac06a6650a
Daniel Jackson <d95jackson@gmail.com>
2019-12-31 14:48:59 +0000

    somewhat working...

--- solutions/day14/main.js
+++ solutions/day14/main.js
@@ -2,6 +2,79 @@ const { getInputs } = require('../../utils/files')
 
 const inputs = getInputs(14)
 
+// 10 ORE => 10 A
+// 7 A, 1 B => 1 C
+const generateChemicalMap = (input) => {
+  const split = input.split('\n').filter(r => r).map(r => r.trim().split(/=>/g))
+
+  const map = split.reduce((acc, [recipe, output]) => {
+    const [outputAmount, outputChemical] = output.trim().split(' ')
+
+    const recipeSplit = recipe.split(',').map(r => {
+      const [amount, chemical] = r.trim().split(' ')
+      return [chemical, Number(amount)]
+    })
+    // console.log('TCL: recipeSplit', recipeSplit)
+
+    acc[outputChemical] = {
+      amount: Number(outputAmount),
+      required: recipeSplit,
+    }
+
+    return acc
+  }, {})
+
+  return map
+}
+
+const getChemicalReplacements = (map, [chemical, count]) => {
+  const raw = []
+  const extras = []
+
+  // console.log('TCL: getChemicalReplacements -> map[chemical]', map, chemical, map[chemical])
+  const { amount, required } = map[chemical]
+
+  while (count >= amount) {
+    count -= amount
+
+    raw.push(...required)
+  }
+
+  if (count !== 0) {
+    extras.push([chemical, count])
+  }
+
+  return [raw, extras]
+}
+
+const calculateFuelCount = (map) => {
+  const chemicalsRaw = [...map.FUEL.required]
+  const chemicalsExtras = []
+
+  while (true) {
+    console.log('TCL: calculateFuelCount -> chemicalsRaw', chemicalsRaw)
+    console.log('TCL: calculateFuelCount -> chemicalsExtras', chemicalsExtras)
+
+    if (chemicalsRaw.length !== 0) {
+      const first = chemicalsRaw.shift()
+      // console.log('TCL: calculateFuelCount -> first', first)
+      // console.log('TCL: calculateFuelCount -> chemicalsRaw', chemicalsRaw)
+
+      const [newRaw, newExtras] = getChemicalReplacements(map, first)
+
+      chemicalsRaw.push(...newRaw)
+      chemicalsExtras.push(...newExtras)
+    }
+  }
+}
+
+const calculateAmountOfOreForFuel = (input) => {
+  const chemicalMap = generateChemicalMap(input)
+
+  const costs = calculateFuelCount(chemicalMap, [[1, 'FUEL']])
+  console.log('TCL: calculateAmountOfOreForFuel -> costs', costs)
+}
+
 const a = () => {
   console.log('a = ?')
 }
@@ -15,4 +88,8 @@ if (runningAsScript) {
   b()
 }
 
-module.exports = {}
+module.exports = {
+  a,
+  b,
+  calculateAmountOfOreForFuel,
+}
--- solutions/day14/main.spec.js
+++ solutions/day14/main.spec.js
@@ -1,9 +1,20 @@
 const chai = require('chai')
 const { expect } = chai
+const { calculateAmountOfOreForFuel } = require('./main')
 
 describe('day 14', () => {
   describe('part a examples', () => {
-    // tests
+    const instructions = `
+    10 ORE => 10 A
+    1 ORE => 1 B
+    7 A, 1 B => 1 C
+    7 A, 1 C => 1 D
+    7 A, 1 D => 1 E
+    7 A, 1 E => 1 FUEL`
+
+    it('should calculate the correct amount of A for FUEL', () => {
+      expect(calculateAmountOfOreForFuel(instructions)).to.equal(31)
+    })
   })
 
   describe('part b examples', () => {
