c70e8db7db25d142808daca69ef08ed0fcbb3694
Daniel Jackson <d95jackson@gmail.com>
2019-12-31 14:49:50 +0000

    objects instead of arrays?

--- solutions/day14/main.js
+++ solutions/day14/main.js
@@ -2,6 +2,19 @@ const { getInputs } = require('../../utils/files')
 
 const inputs = getInputs(14)
 
+function mergeOptions (obj1, obj2) {
+  const obj3 = {}
+  for (const attrname in obj1) { obj3[attrname] = obj1[attrname] }
+  for (const attrname in obj2) {
+    if (!obj3[attrname]) {
+      obj3[attrname] = obj2[attrname]
+    } else {
+      obj3[attrname] += obj2[attrname]
+    }
+  }
+  return obj3
+}
+
 // 10 ORE => 10 A
 // 7 A, 1 B => 1 C
 const generateChemicalMap = (input) => {
@@ -10,10 +23,11 @@ const generateChemicalMap = (input) => {
   const map = split.reduce((acc, [recipe, output]) => {
     const [outputAmount, outputChemical] = output.trim().split(' ')
 
-    const recipeSplit = recipe.split(',').map(r => {
+    const recipeSplit = recipe.split(',').reduce((acc, r) => {
       const [amount, chemical] = r.trim().split(' ')
-      return [chemical, Number(amount)]
-    })
+      acc[chemical] = Number(amount)
+      return acc
+    }, {})
     // console.log('TCL: recipeSplit', recipeSplit)
 
     acc[outputChemical] = {
@@ -27,43 +41,50 @@ const generateChemicalMap = (input) => {
   return map
 }
 
-const getChemicalReplacements = (map, [chemical, count]) => {
-  const raw = []
-  const extras = []
+const getChemicalReplacements = (map, chemicalCount) => {
+  const [chemical] = Object.keys(chemicalCount)
+  let count = chemicalCount[chemical]
 
-  // console.log('TCL: getChemicalReplacements -> map[chemical]', map, chemical, map[chemical])
+  let raw = {}
+  let extras = {}
+
   const { amount, required } = map[chemical]
 
   while (count >= amount) {
     count -= amount
 
-    raw.push(...required)
+    raw = mergeOptions(raw, required)
   }
 
   if (count !== 0) {
-    extras.push([chemical, count])
+    extras = mergeOptions(raw, { [chemical]: count })
   }
 
   return [raw, extras]
 }
 
 const calculateFuelCount = (map) => {
-  const chemicalsRaw = [...map.FUEL.required]
-  const chemicalsExtras = []
+  let chemicalsRaw = { ...map.FUEL.required }
+  let chemicalsExtras = {}
 
   while (true) {
     console.log('TCL: calculateFuelCount -> chemicalsRaw', chemicalsRaw)
     console.log('TCL: calculateFuelCount -> chemicalsExtras', chemicalsExtras)
 
     if (chemicalsRaw.length !== 0) {
-      const first = chemicalsRaw.shift()
-      // console.log('TCL: calculateFuelCount -> first', first)
+      const [firstKey] = Object.keys(chemicalsRaw) // .shift()
+
+      const first = { [firstKey]: chemicalsRaw[firstKey] }
+      delete chemicalsRaw[firstKey]
+
+      console.log('TCL: calculateFuelCount -> first', first)
       // console.log('TCL: calculateFuelCount -> chemicalsRaw', chemicalsRaw)
 
       const [newRaw, newExtras] = getChemicalReplacements(map, first)
+      console.log('TCL: calculateFuelCount -> [newRaw, newExtras] ', [newRaw, newExtras])
 
-      chemicalsRaw.push(...newRaw)
-      chemicalsExtras.push(...newExtras)
+      chemicalsRaw = mergeOptions(chemicalsRaw, newRaw)
+      chemicalsExtras = mergeOptions(chemicalsExtras, newExtras)
     }
   }
 }
